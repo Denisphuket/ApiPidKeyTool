f7937fac0961e793d7d480f47fad8c207007ed37
Denisphuket <taxiphuket@yandex.ru>
2023-11-15 09:05:20 +0700

    modified:   ApiPidKeyTool/Models/KeyResponse.cs

--- ApiPidKeyTool/Models/KeyResponse.cs
+++ ApiPidKeyTool/Models/KeyResponse.cs
@@ -1,8 +1,8 @@
 public class KeyResponse
 {
-    public string Key { get; set; }
-    public string Description { get; set; }
-    public string Remain { get; set; }
-    public string ErrorCode { get; set; }
-    public string IID { get; set; }
+    public string? Key { get; set; }
+    public string? Description { get; set; }
+    public string? Remain { get; set; }
+    public string? ErrorCode { get; set; }
+    public string? IID { get; set; }
 }
